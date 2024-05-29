e77a7013a86d4aa5f4cdfec7eb7c7e34a9416df9
unknown <kunyzee@gmail.com>
2024-05-29 15:04:39 +0300

    new script for price compare

--- autosync-suppliers/functions/ml_price_compare.py
+++ autosync-suppliers/functions/ml_price_compare.py
@@ -26,34 +26,33 @@ class SubCategory:
     PERIPHERALS = (
         'כרטיסי PCI וPCIE', 'משטח קירור למחשב נייד', 'מוצרי אבטחה', 'מפצלי USB', 'מצלמות אינטרנט', 'ציוד נלווה לMINING',
     )
-    CABLE_PERIPHERALS = 'כבלים ומתאמים'
+    CABLE_PERIPHERALS = ('כבלים ומתאמים')
     CPUS = ('מעבדים INTEL', 'מעבדים AMD')
-    LIQUID_COOLING = 'קירור נוזלי'
-    AIR_COOLING = 'פתרונות קירור אוויר'
-    THERMO_PASTE = 'משחה טרמית'
+    LIQUID_COOLING = ('קירור נוזלי')
+    AIR_COOLING = ('פתרונות קירור אוויר')
+    THERMO_PASTE = ('משחה טרמית')
     MEMORY = ('זכרון לנייח DIMM', 'זכרון לנייד SODIMM')
-    INTERNAL_STORAGE = ('SSD sata 3', 'NVME GEN3', 'NVME GEN4', 'HDD פנימי')
+    INTERNAL_STORAGE = ('SSD sata 3', 'NVME GEN3', 'NVME GEN4', 'HDD פנימי','NVME GEN5')
     EXTERNAL_STORAGE = ('קופסא חיצונית לHDD', 'SSD חיצוני', 'HDD חיצוני', 'Disk On Key', 'MICRO SD', 'כונן אופטי')
     PC_CASES = ('Antec', 'Corsair', 'Cooler Master', 'Solid & Others')
-    PC_FANS = 'מאוררים למארז'
-    PC_INTERNAL_CABLES = 'כבלים ומתאמים לבניית מחשב'
-    PC_PANELS_AND_DOORS = 'פנלים קדמיים ודלתות צד'
-    PSUS = 'ספקי כוח'
-    LAPTOPS_TABLETS = 'מחשבים ניידים'
+    PC_FANS = ('מאוררים למארז')
+    PC_INTERNAL_CABLES = ('כבלים ומתאמים לבניית מחשב')
+    PC_PANELS_AND_DOORS = ('פנלים קדמיים ודלתות צד')
+    PSUS = ('ספקי כוח')
+    LAPTOPS_TABLETS = ('מחשבים ניידים')
     WARRANTY_EXT = ('הרחבות אחריות ניידים', 'הרחבת אחריות למחשבי מותג')
-    CHARGERS = 'מטענים'
-    DOCKING_STATIONS = 'תחנות עגינה'
-    BAGS = 'תיקים'
+    CHARGERS = ('מטענים')
+    DOCKING_STATIONS = ('תחנות עגינה')
+    BAGS = ('תיקים')
     DESKTOPS = ('Mini PC', 'AIO', 'מחשב מותג')
     KBM = ('מקלדות סטנדרטיות', 'מקלדות גיימינג', 'עכברים', 'סט מקלדת + עכבר', 'משטח לעכבר/משענת יד למקלדת')
     MONITORS_TVS = ('מסכי מחשב', 'טלוויזיות')
-    SCREEN_HANGERS = 'מתקני תלייה'
+    SCREEN_HANGERS = ('מתקני תלייה')
     SOUND = ('רמקולים', 'מיקרופונים', 'אוזניות')
-    UPS = 'אל פסק UPS'
+    UPS = ('אל פסק UPS')
     HOME_NETWORK = ("סוויצ'ים", "כרטיסי רשת", "מודם סלולרי", "אקסס פוינט", "נתבים")
     COMM_CLOSET = ('ארונות תקשורת', 'כלי עבודה לציוד רשת')
     NETWORK_CABLES = ('כבלי רשת', 'ראשים וקופסאות')
-
 
 
 def change_prices_mor_levi(df):
@@ -72,6 +71,10 @@ def change_inventory(df):
 def change_item_status(df):
     df['ItemStatus'] = df['ItemStatus'].apply(lambda x: True)
     return df
+
+
+def delete_irrelevant_categorys(df):
+    return df[df['Category'] != Category.CCTV]
 
 
 def finalize_sale_price(df):
@@ -97,6 +100,7 @@ def calculate_sale_price(row):
                 return round((row['CostPrice'] * 1.17 * 1.02 * 1.3), 0)
             elif row['SubCategory'] in SubCategory.CABLE_PERIPHERALS:
                 return round((row['CostPrice'] * 2.5), 0)
+
         case Category.CPU_AND_COOLING:
             if row['SubCategory'] in SubCategory.CPUS:
                 if row['CostPrice'] < 1000:
@@ -109,27 +113,71 @@ def calculate_sale_price(row):
                 return round((row['CostPrice'] * 1.17 * 1.02 * 1.15), 0)
             if row['SubCategory'] in SubCategory.THERMO_PASTE:
                 return round((row['CostPrice'] * 2), 0)
+
         case Category.MEMORY:
-            pass
+            if row['SubCategory'] in SubCategory.MEMORY:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.2), 0)
+
         case Category.STORAGE:
-            pass
+            if row['SubCategory'] in SubCategory.INTERNAL_STORAGE:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.15), 0)
+            elif row['SubCategory'] in SubCategory.EXTERNAL_STORAGE:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.3), 0)
+
         case Category.FANS_CASES_MISC:
-            pass
+            if row['SubCategory'] in SubCategory.PC_CASES:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.5), 0)
+            elif row['SubCategory'] in SubCategory.PC_FANS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.5), 0)
+            elif row['SubCategory'] in SubCategory.PC_INTERNAL_CABLES:
+                return round((row['CostPrice'] * 2.5), 0)
+            elif row['SubCategory'] in SubCategory.PC_PANELS_AND_DOORS:
+                return round((row['CostPrice'] * 2), 0)
+
         case Category.PSU:
-            pass
+            if row['SubCategory'] in SubCategory.PSUS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.15), 0)
+
         case Category.LAPTOPS_TABLETS:
-            pass
+            if row['SubCategory'] in SubCategory.LAPTOPS_TABLETS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.05), 0)
+            elif row['SubCategory'] in SubCategory.WARRANTY_EXT:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.5), 0)
+            elif row['SubCategory'] in SubCategory.CHARGERS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.5), 0)
+            elif row['SubCategory'] in SubCategory.DOCKING_STATIONS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.3), 0)
+            elif row['SubCategory'] in SubCategory.BAGS:
+                return round((row['CostPrice'] * 2.5), 0)
+
         case Category.DESKTOPS:
-            pass
+            if row['SubCategory'] in SubCategory.DESKTOPS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.1), 0)
+            elif row['SubCategory'] in SubCategory.WARRANTY_EXT:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.5), 0)
+
         case Category.KBM:
-            pass
+            if row['SubCategory'] in SubCategory.KBM:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.3), 0)
+
         case Category.MONITORS_TVS_AND_HANGERS:
-            pass
+            if row['SubCategory'] in SubCategory.MONITORS_TVS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.14), 0)
+            elif row['SubCategory'] in SubCategory.SCREEN_HANGERS:
+                return round((row['CostPrice'] * 2), 0)
+
         case Category.SOUND:
-            pass
+            if row['SubCategory'] in SubCategory.SOUND:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.3), 0)
+
         case Category.NETWORK:
-            pass
+            if row['SubCategory'] in SubCategory.HOME_NETWORK:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.25), 0)
+            elif row['SubCategory'] in SubCategory.COMM_CLOSET:
+                return round((row['CostPrice'] * 2), 0)
+            elif row['SubCategory'] in SubCategory.NETWORK_CABLES:
+                return round((row['CostPrice'] * 3.5), 0)
+
         case Category.UPS:
-            pass
-        case Category.CCTV:
-            pass
+            if row['SubCategory'] in SubCategory.UPS:
+                return round((row['CostPrice'] * 1.17 * 1.02 * 1.3), 0)
--- autosync-suppliers/ml_compare.py
+++ autosync-suppliers/ml_compare.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from functions.ml_price_compare import change_inventory, change_prices_mor_levi, change_item_status, \
-    finalize_sale_price
+    finalize_sale_price,delete_irrelevant_categorys
 
 df = pd.read_csv("files/Mor_Levi_Compare_FINAL.csv")
 mor_levi = df.copy()
 
 mor_levi = change_prices_mor_levi(mor_levi)
+mor_levi = delete_irrelevant_categorys(mor_levi)
 mor_levi = finalize_sale_price(mor_levi)
 mor_levi = change_inventory(mor_levi)
 mor_levi = change_item_status(mor_levi)
