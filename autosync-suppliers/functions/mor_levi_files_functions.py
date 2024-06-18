db4512426452e7250355b47d05ae4a7ae4b15e28
unknown <kunyzee@gmail.com>
2024-06-18 13:24:18 +0300

    reorder files structure

--- autosync-suppliers/functions/mor_levi_files_functions.py
+++ autosync-suppliers/functions/mor_levi_files_functions.py
@@ -1,11 +1,9 @@
 import pandas as pd
-
-from functions.mor_levi_compare_functions import change_prices_mor_levi, delete_irrelevant_categorys, finalize_sale_price, \
-    change_inventory, change_item_status
+from functions.mor_levi_compare_functions import change_prices_mor_levi, delete_irrelevant_categorys, finalize_sale_price, change_inventory, change_item_status
 
 
-def create_updated_file(filepath):
-    df = pd.read_csv(filepath)
+def create_updated_file(supplier_file_path):
+    df = pd.read_csv(supplier_file_path)
     mor_levi = df.copy()
 
     mor_levi = change_prices_mor_levi(mor_levi)
@@ -16,5 +14,15 @@ def create_updated_file(filepath):
     mor_levi.to_csv("mor_levi_compare_updated.csv", encoding='utf-8-sig', index=False)
 
 
-def create_deleted_file(filepath):
-    pass
+def create_deleted_file(supplier_file_path):
+    df = pd.read_csv(supplier_file_path)
+    mor_levi = df.copy()
+    return mor_levi
+
+
+def compare_mor_levi_files(supplier_file_path, aio_website_file_path):
+    df = pd.read_csv(supplier_file_path)
+    df2 = pd.read_csv(aio_website_file_path)
+    mor_levi = df.copy()
+    all_in_one = df2.copy()
+    return mor_levi, all_in_one
